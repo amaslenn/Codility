6eba59d51c80df79859a5d65578406f52e7bc015
Maslennikov, Andrey <andrew.maslennikov@gmail.com>
2016-12-14 23:34:46 +0300

    Start working on GenomicRangeQuery

---
+++ Lesson 5 - Prefix Sums/GenomicRangeQuery.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import unittest
+
+
+def solution():
+    pass
+
+
+class Tests(unittest.TestCase):
+    """Tests for GenomicRangeQuery solution"""
+    def test_from_task(self):
+        """Sample from task description"""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main(failfast=True)
