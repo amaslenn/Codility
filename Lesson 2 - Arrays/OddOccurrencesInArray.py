80522751f5e84d1b6c7b512aa13872077e22cf40
Maslennikov, Andrey <andrew.maslennikov@gmail.com>
2017-10-16 20:01:29 +0300

    Remove useless test
    Test tried to measure time, but it heavily depends on HW.

--- Lesson 2 - Arrays/OddOccurrencesInArray.py
+++ Lesson 2 - Arrays/OddOccurrencesInArray.py
@@ -20,15 +20,6 @@ class Tests(unittest.TestCase):
         """Sample array from task description"""
         self.assertEqual(7, solution([9, 3, 9, 3, 9, 7, 9]))
 
-    def test_time(self):
-        """Array of 1000000 elements, check time limit"""
-        array = [1 for x in range(1000000)]
-        array[0] = 2
-        s = time.time()
-        solution(array)
-        e = time.time()
-        self.assertLess(e-s, 0.15)
-
 
 if __name__ == '__main__':
     unittest.main()
