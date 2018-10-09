eb7a6a655c2e038772a1dc00fc49c7ed7a51a9c4
Maslennikov, Andrey <andrew.maslennikov@gmail.com>
2018-10-09 07:56:19 +0300

    Add more tests, fix issues

--- Lesson 6 - Sorting/MaxProductOfThree.py
+++ Lesson 6 - Sorting/MaxProductOfThree.py
@@ -8,9 +8,12 @@ def solution(A):
     S = sorted(A)
     prod = 1
     if S[-1] > 0:
-        prod = S[-3] * S[-2] * S[-1]
-        if abs(sum(S[:2])) > abs(sum(S[-2:])):
+        if S[-2] < 0:
             prod = S[0] * S[1] * S[-1]
+        else:
+            p1 = S[0] * S[1] * S[-1]
+            p2 = S[-3] * S[-2] * S[-1]
+            prod = max(p1, p2)
     else:
         for x in S[-3:]:
             prod *= x
@@ -39,6 +42,14 @@ class Tests(unittest.TestCase):
     def test_one_pos(self):
         """The only positive"""
         self.assertEqual(26, solution([-13, -2, -1, 1]))
+
+    def test_zero(self):
+        """Let's have a zero!"""
+        self.assertEqual(0, solution([-13, -2, -1, 0]))
+
+    def test_zero2(self):
+        """Let's have a zero 2"""
+        self.assertEqual(13*2*27, solution([-13, -2, -1, 0, 27]))
 
 
 if __name__ == '__main__':
